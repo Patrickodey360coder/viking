bb71ae5c58dac0de51890f6407f2ac25a234449f
patrick <patrickodey419@gmail.com>
2021-12-15 22:17:41 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/styles.css';
+import './js/scripts';
+import { Routes, Route, Router } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Main from './components/Main';
+import SideNav from './components/SideNav';
+import Footer from './components/Footer';
+import Register from './pages/Register';
+import Error404 from './pages/404';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="sb-nav-fixed">
+      <Navbar />
+
+      <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+          <SideNav />
+        </div>
+
+        <div id="layoutSidenav_content">
+          <Main>
+            <Router>
+              <Routes>
+                <Route path="/register" element={ <Register /> } /> 
+                
+                <Route path="*" element={ <Error404/> } />
+              </Routes>
+            </Router>
+          </Main>
+          <Footer />
+        </div>        
+      </div>
+
+
     </div>
+
   );
 }
 
