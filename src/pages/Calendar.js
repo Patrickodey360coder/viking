c6e198ea36258afd91ee127891b6b4559b2bb788
patrick <patrickodey419@gmail.com>
2021-12-19 13:30:22 +0100

    added calendar

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 function MyCalendar() {
   const [value, onChange] = useState(new Date());
 
   return (
-    <>
+    <div style={{width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
+      <div>
       <h1 className='text-center'>Calendar</h1>
-      <Calendar
-        onChange={onChange}
-        value={value}
-      />
-    </>
+        <Calendar 
+          onChange={onChange}
+          value={value}
+        />
+      </div>
+    </div>
   )
 }
 
